c33c1d6b516f721dc9f74fe4fa16d1073264d7f3
0hmori <mehowomori.fiofio.cute.38@gmail.com>
2023-09-25 06:53:09 +0900

    c-7

--- customer.py
+++ customer.py
@@ -84,3 +84,67 @@ tom.info_csv()  # "Tom Ford,57,1500" という値を返す
 
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
+
+
+# c-5
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+
+    def entry_fee(self):
+        if self.age <= 3:
+            print(0)
+        elif 3 < self.age < 20:
+            print(1000)
+        elif 20 <= self.age < 65:
+            print(1500)
+        else:
+            print(1200)
+
+#確認用
+ken = Customer(first_name="Ken", family_name="Tanaka", age=3)
+ken.entry_fee()
+
+
+# c-6
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+
+    def entry_fee(self):
+        if self.age <= 3:
+            print(0)
+        elif 3 < self.age < 20:
+            print(1000)
+        elif 20 <= self.age < 65:
+            print(1500)
+        elif 65 <= self.age < 75:
+            print(1200)
+        else:
+            print(500)
+
+#確認用
+ken = Customer(first_name="Ken", family_name="Tanaka", age=75)
+ken.entry_fee()
+
+
+# c-7
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+
+    def info_csv(self):
+        if self.age < 20:
+            print(self.first_name + " " + self.family_name, self.age, str(1000), sep="\t")
+        elif 20 <= self.age < 65:
+            print(self.first_name + " " + self.family_name, self.age, str(1500), sep="\t")
+        else:
+            print(self.first_name + " " + self.family_name, self.age, str(1200), sep="\t")
+
+#c-8は未完成
