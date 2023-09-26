9b1b74135f14f4ad9546671acd505a584783d11a
0hmori <mehowomori.fiofio.cute.38@gmail.com>
2023-09-26 20:09:37 +0900

    C-8

---
+++ c-8.py
@@ -0,0 +1,24 @@
+# c-8
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+
+    def full_name(self):
+        return self.first_name + " " + self.family_name
+
+    def entry_fee(self):
+        if self.age < 20:
+            return 1000
+        elif 20 <= self.age < 65:
+            return 1500
+        else:
+            return 1200
+
+    def pipe_delimited(self):
+        print(f"{self.full_name()}|{self.age}|{self.entry_fee()} ")
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=75)
+ken.pipe_delimited()
